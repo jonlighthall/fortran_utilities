a2932d03f53be4b4187d6a4c057a96e0f1a69203
Jon Lighthall <jon.lighthall@gmail.com>
2024-03-04 15:13:20 -0600

    use bash links

--- make_links.sh
+++ make_links.sh
@@ -1,18 +1,33 @@
 #!/bin/bash -u
 
+# get starting time in nanoseconds
+start_time=$(date +%s%N)
+
+utils_dir="${HOME}/utils"
+bash_utils_dir="${utils_dir}/bash"
+
 # load formatting
-fpretty="${HOME}/utils/bash/.bashrc_pretty"
+fpretty="${bash_utils_dir}/.bashrc_pretty"
 if [ -e "$fpretty" ]; then
     source "$fpretty"
+    set_traps
+    # reset tab
+    rtab
+fi
+
+# load linking scripts
+flink="${bash_utils_dir}/.bash_links"
+if [ -e "$flink" ]; then
+    source "$flink"
 fi
 
 # determine if script is being sourced or executed
 if (return 0 2>/dev/null); then
     RUN_TYPE="sourcing"
 else
-	RUN_TYPE="executing"
-	# exit on errors
-	set -e
+    RUN_TYPE="executing"
+    # exit on errors
+    set -e
 fi
 # print source name at start
 echo -e "${TAB}${RUN_TYPE} ${PSDIR}$BASH_SOURCE${NORMAL}..."
@@ -24,12 +39,14 @@ fi
 # generate executables before linking
 make
 
-# set source and target directories
-target_dir=$(dirname "$src_name")/bin
+# set target and link directories
+src_dir_logi=$(dirname "$src_name")
+proj_name=$(basename "$src_dir_logi")
+target_dir="${utils_dir}/${proj_name}/bin"
 link_dir=$HOME/bin
 
 # check directories
-echo -n "source directory ${target_dir}... "
+echo -n "target directory ${target_dir}... "
 if [ -d "$target_dir" ]; then
     echo "exists"
 else
@@ -50,82 +67,15 @@ bar 38 "------ Start Linking Repo Files-------"
 # list of files to be linked
 ext=.exe
 for my_link in cpddiff \
-				   prsdiff \
-				   tldiff \
-				   tsdiff
-do
-	# define target (source)
-	target=${target_dir}/${my_link}${ext}
-	# define link (destination)
+    prsdiff \
+    tldiff \
+    tsdiff; do
+    # define target (source)
+    target=${target_dir}/${my_link}${ext}
+    # define link (destination)
     link=${link_dir}/${my_link}
+    echo "linking $target to $link..."
 
-	# check if target exists
-	echo -n "target file ${target}... "
-    if [ -e "${target}" ]; then
-		echo -n "exists and "
-		permOK=500
-		perm=$(stat -c "%a" "${target}")
-		if [[ ${perm} -ge ${permOK} ]]; then
-			if [ -x "${target}" ]; then
-				echo -n "is"
-			else
-				echo -n "should be"
-			fi
-			echo " executable"
-			# begin linking...
-			echo -n "${TAB}link ${link}... "
-			TAB+=${fTAB:='   '}
-			# first, check for existing copy
-			if [ -L "${link}" ] || [ -f "${link}" ] || [ -d "${link}" ]; then
-				echo -n "exists and "
-				if [[ "${target}" -ef "${link}" ]]; then
-					echo "already points to ${my_link}"
-					echo -n "${TAB}"
-					ls -lhG --color=auto "${link}"
-					echo "${TAB}skipping..."
-					TAB=${TAB%$fTAB}
-					continue
-				else
-					# next, delete or backup existing copy
-					if [ $(diff -ebwB "${target}" "${link}" 2>&1 | wc -c) -eq 0 ]; then
-						echo "has the same contents"
-						echo -n "${TAB}deleting... "
-						rm -v "${link}"
-					else
-						if [ -e "${link}" ]; then
-                        echo "will be backed up..."
-                        mdate=$(date -r "${link}" +'%Y-%m-%d-t%H%M')
-                    else
-                        echo -n "is a broken link..."
-                        mdate=$(stat -c '%y' ${link} | sed 's/\(^[0-9-]*\) \([0-9:]*\)\..*$/\1-t\2/' | sed 's/://g')
-                    fi
-                    link_copy="${link}_${mdate}"
-                    mv -v "${link}" "${link_copy}" | sed "s/^/${TAB}/"
-					fi
-				fi
-			else
-				echo "does not exist"
-			fi
-			# then link
-			echo -en "${TAB}${GRH}"
-			hline 72
-			echo "${TAB}making link... "
-			ln -sv "${target}" "${link}" | sed "s/^/${TAB}/"
-			echo -ne "${TAB}"
-			hline 72
-			echo -en "${NORMAL}"
-			TAB=${TAB%$fTAB}		else
-			echo -e "${BAD}not executable${NORMAL}"
-        fi
-	else
-		echo -e "${BAD}does not exist${NORMAL}"
-	fi
+    do_link_exe "$target" "$link"
 done
 bar 38 "--------- Done Making Links ----------"
-# print time at exit
-echo -en "$(date +"%a %b %-d %-l:%M %p %Z") ${BASH_SOURCE##*/} "
-if command -v sec2elap &>/dev/null; then
-	bash sec2elap $SECONDS
-else
-	echo "elapsed time is ${SECONDS} sec"
-fi
