5bd6578491b9256fdce0aa7fa787463a23b6cdbf
Jon Lighthall <jonathan.c.lighthall@us.navy.mil>
2024-07-11 11:25:56 -0500

    update variable names, add error check, add formatting, add comments

--- tldiff.f90
+++ tldiff.f90
@@ -13,16 +13,32 @@ program tldiff
   ! Aug 2022 JCL
   implicit none
   integer,parameter :: srk = selected_real_kind(2)
+  ! range
   real(kind=srk), dimension(:), allocatable :: r1,r2
+  ! TL
   real(kind=srk), dimension(:,:), allocatable :: tl1,tl2
-  integer :: i,n1,io,ln1,ln2,unit1,unit2,n2,ln3,nerr,ns1,j,ls,ns2,nerr2,nerr3
-  character(len=256) :: fname1, fname2, tlthresh,dummy
-  real(kind=srk)::dtl,dtl_max
+  real(kind=srk)::dtl,dtl_max=0
+  ! arguments
+  integer :: ln1,ln2,ln3
+  ! file parameters
+  integer :: io,ls,n1,n2,ns1,ns2,unit1,unit2
+  character(len=256) :: dummy,fname1,fname2,tlthresh
+  ! counters
+  integer :: i,j
+  integer :: nerr=0,nerr2=0,nerr3=0
   ! ----------------------------------------------------------
   ! set thresholds
-  real(kind=srk),parameter :: rdiff=0.01
-  real(kind=srk)::dtl_print=0.01
-  real(kind=srk),parameter :: tl_red=0.1, comp_diff=0.001
+
+  ! see ramio/outpt.f, format 20
+  ! ranges are formatted as f12.2
+  ! the minimum difference between ranges is therefore 0.01
+  real(kind=srk),parameter :: rng_min_diff=0.01
+  ! TL is formmated as f6.1
+  ! the minimum difference between ranges is therefore 0.1
+  real(kind=srk),parameter :: tl_min_diff=0.1
+
+  real(kind=srk),parameter :: tl_red=0.1
+  real(kind=srk),parameter :: comp_diff=0.001
   real(kind=srk),parameter :: tlmax=-20*log10(2.**(-23))
   real(kind=srk)::dtl_error
   ! ----------------------------------------------------------
@@ -30,13 +46,15 @@ program tldiff
   call get_command_argument(2,fname2,ln2)
   call get_command_argument(3,tlthresh,ln3)
 100 format(a,f7.3)
+  print 100,' rn dif = ',rng_min_diff
+  print 100,' tl dif = ',tl_min_diff
   print 100,' tl max = ',tlmax
   print 100,' tl red = ',tl_red
   print 100,' tl com = ',comp_diff
   dtl_error=tl_red+comp_diff
   print 100,' tl err = ',dtl_error
 
-  !     set file names
+  ! set file names
   if (ln1.eq.0) then
      fname1='nspe02.asc'
   end if
@@ -45,12 +63,21 @@ program tldiff
   end if
 
   if (ln3.gt.0) then
-     read(tlthresh,*)dtl_print
-     print *, 'using user-defined dtl_print'
+     !read(tlthresh,*)tl_min_diff
+     print *, 'using user-defined tl_min_diff'
   else
-     print *, 'using default dtl_print'
+     print *, 'using default tl_min_diff'
   end if
-  print 100,' tl dif = ',dtl_print
+  ! if (tl_min_diff.gt.tl_red) then
+  !    print *, 'adjusting tl_min_diff'
+  !    tl_min_diff=tl_min_diff-comp_diff
+  ! endif
+
+  if (tl_min_diff.gt.dtl_error) then
+     print *, 'tl_min_diff < dtl_error'
+     print *, 'error values invalid'
+     STOP 1
+  endif
 
   !     open files
   open (newunit=unit1, file = fname1, status = 'old')
@@ -124,18 +151,21 @@ program tldiff
      stop 1
   endif
 
-  !     read file
+  ! read file 1
   allocate(r1(n1),tl1(n1,ns1))
   rewind(unit1)
+  ! loop over lines
   do i = 1,n1
      read(unit1,*) r1(i), (tl1(i,j), j=1,ns1)
   end do
   close(unit1)
+  ! read file 2 and compare ranges to file 1
   allocate(r2(n1),tl2(n1,ns1))
   rewind(unit2)
+  ! loop over lines
   do i = 1,n1
      read(unit2,*) r2(i), (tl2(i,j),j=1,ns1)
-     if (abs(r1(i)-r2(i)).gt.rdiff) then
+     if (abs(r1(i)-r2(i)).gt.rng_min_diff) then
         print *, 'ranges do not match'
         print *, 'range ',i,' file 1 = ',i,r1(i)
         print *, 'range ',i,' file 2 = ',i,r2(i)
@@ -147,14 +177,13 @@ program tldiff
   close(unit2)
   print *, 'ranges match'
 
+  ! print summary
   print *, 'tl1 = ',trim(fname1)
   print *, 'tl2 = ',trim(fname2)
 
-  ! print summary
-  nerr=0
-  nerr2=0
-  nerr3=0
   dtl_max=0
+101 format(a,f4.1,a)
+  
   ! loop over lines
   do i = 1,n1
      ! loop over columns
@@ -163,8 +192,8 @@ program tldiff
         dtl=abs(tl1(i,j)-tl2(i,j))
         ! compare to maximum difference in TL
         if (dtl.gt.dtl_max) dtl_max=dtl
-        ! compare to dtl_print
-        if(dtl.gt.dtl_print) then
+        ! compare to tl_min_diff
+        if(dtl.gt.tl_min_diff) then
            if (nerr.eq.0) then ! print table header on first error
               print'(/a)','   ix   iz    range    tl1    tl2 | diff'
               print*, repeat('-',33),'+',repeat('-',6)
@@ -186,13 +215,13 @@ program tldiff
 
            if(dtl.gt.(dtl_error)) then
               if((tl1(i,j).gt.tlmax).or.(tl2(i,j).gt.tlmax))then
-                 print '(a,f4.1,a)',''//achar(27)//'[33m',dtl,''//achar(27)//'[0m'
+                 print 101,''//achar(27)//'[33m',dtl,''//achar(27)//'[0m'
               else
-                 print '(a,f4.1,a)',''//achar(27)//'[31m',dtl,''//achar(27)//'[0m'
+                 print 101,''//achar(27)//'[31m',dtl,''//achar(27)//'[0m'
               endif
               nerr2=nerr2+1
            else
-              write(*,'(f4.1)') dtl
+              write(*,101) '',dtl,''
            endif
 
            if((tl1(i,j).lt.tlmax).and.(tl2(i,j).lt.tlmax).and.(dtl.gt.(dtl_error)))then
@@ -201,8 +230,8 @@ program tldiff
         endif
      enddo
   enddo
-  print '(/a,f6.3,a,i0)',' number of errors found (>',dtl_print,'): ',nerr
-  if(tl_red.ge.dtl_print) then
+  print '(/a,f6.3,a,i0)',' number of errors found (>',tl_min_diff,'): ',nerr
+  if(tl_red.ge.tl_min_diff) then
      print '(a,f6.3,a,i0)',' number of errors found (>',dtl_error,'): ',nerr2
      print '(a,f6.3,a,f5.1,a,i0)',' number of errors found (>',dtl_error,' and tl < ',tlmax,'): ',nerr3
   endif
